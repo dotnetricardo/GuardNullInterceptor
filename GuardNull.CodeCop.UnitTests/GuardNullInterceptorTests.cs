4d6df767984defc6f0c4d0db28a47d8da6ff46a2
dotnetricardo <dotnetricardo@hotmail.com>
2015-09-04 09:31:56 +0100

    Initial Commit

---
+++ GuardNull.CodeCop.UnitTests/GuardNullInterceptorTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using CodeCop.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace GuardNull.CodeCop.UnitTests
+{
+    [TestClass]
+    public class GuardNullInterceptorTests
+    {
+        [TestMethod]
+        public void GuardNullShouldThrowArgumentNullExceptionWhenArgumentIsNull()
+        {
+            // Arrange
+            var gni = new GuardNullInterceptor();
+            var context = new InterceptionContext
+            {
+                Parameters = new List<Parameter> {new Parameter() {Name = "Dummy", Value = null}}
+            };
+
+            // Act & Assert
+            gni.Invoking(x => gni.OnBeforeExecute(context)).ShouldThrow<ArgumentNullException>();
+
+        }
+
+        [TestMethod]
+        public void GuardNullShouldThrowArgumentNullExceptionWhenArgumentTypeIsStringAndIsEmpty()
+        {
+            // Arrange
+            var gni = new GuardNullInterceptor();
+            var context = new InterceptionContext
+            {
+                Parameters = new List<Parameter> {new Parameter() {Name = "Dummy", Value = ""}}
+            };
+
+            // Act & Assert
+            gni.Invoking(x => gni.OnBeforeExecute(context)).ShouldThrow<ArgumentNullException>();
+
+        }
+
+        [TestMethod]
+        public void GuardNullShouldNotThrowArgumentNullExceptionWhenParameterIsOptional()
+        {
+            // Arrange
+            var gni = new GuardNullInterceptor();
+            var context = new InterceptionContext
+            {
+                Parameters = new List<Parameter> {new Parameter() {Name = "Dummy", Value = null, IsOptional = true}}
+            };
+
+            // Act & Assert
+            gni.Invoking(x => gni.OnBeforeExecute(context)).ShouldNotThrow<ArgumentNullException>();
+
+        }
+
+        [TestMethod]
+        public void GuardNullShouldThrowArgumentNullExceptionWhenFirstParameterIsNullAndHasOptionalParameter()
+        {
+            // Arrange
+            var gni = new GuardNullInterceptor();
+            var context = new InterceptionContext
+            {
+                Parameters = new List<Parameter> { new Parameter() { Name = "Dummy", Value = null}, new Parameter() { Name = "Dummy2", Value = null, IsOptional = true } }
+            };
+
+            // Act & Assert
+            gni.Invoking(x => gni.OnBeforeExecute(context)).ShouldThrow<ArgumentNullException>();
+
+        }
+    }
+}
---
+++ GuardNull.CodeCop/GuardNullInterceptor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeCop.Core;
+using CodeCop.Core.Contracts;
+
+namespace GuardNull.CodeCop
+{
+    public class GuardNullInterceptor : ICopIntercept
+    {
+        public void OnBeforeExecute(InterceptionContext context)
+        {
+            foreach (var parameter in context.Parameters)
+            {
+                if (!parameter.IsOptional && (parameter.Value == null || (parameter.Value is string && string.IsNullOrEmpty(parameter.Value.ToString()))))
+                {
+                    throw new ArgumentNullException(parameter.Name);
+                }
+            }
+        }
+
+        public void OnAfterExecute(InterceptionContext context)
+        {
+
+        }
+    }
+}
